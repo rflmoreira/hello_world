a0ec0b8cf36e42c8d40bf15709f0f528adb9d121
rafaelmoreira-lima <rafaelmoreira.lima@hotmail.com>
2022-03-23 23:22:11 -0300

    hello_world

---
+++ controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloController {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloController.class, args);
+	}
+
+}
